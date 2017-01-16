1d8eb1b0bebff6fc42b103c7e965551e601a1b65
Dimitriio <dimitri.reynie@gmail.com>
2017-01-16 22:15:25 +0100

    merge

--- Bomberman/src/ui/EndGameView.java
+++ Bomberman/src/ui/EndGameView.java
@@ -1,6 +1,5 @@
 package ui;
 
-import java.awt.Button;
 import java.awt.CardLayout;
 import java.awt.Component;
 import java.awt.Dimension;
@@ -10,7 +9,9 @@ import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.font.FontRenderContext;
+import java.awt.image.BufferedImage;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -20,26 +21,32 @@ public class EndGameView extends JPanel {
 
 	private final int BUTTON_SIZE_WIDTH = 100;
 	private final int BUTTON_SIZE_HEIGHT = 30;
-	
+	public static final Graphics2D DEFAULT_GRAPHICS = (Graphics2D) new BufferedImage(1,1,BufferedImage.TYPE_INT_ARGB).getGraphics();
+
 	
 	private Model model;
-	private final Button restartButton;
-	private final Button exitButton;
+	private final JButton restartButton;
+	private final JButton exitButton;
 	private final JLabel message;
 	
-
 	public EndGameView(Model _model, JPanel cardPanel){
 		this.setLayout(null);
 		this.model = _model;
 		this.setPreferredSize(new Dimension(model.getMap().getWidth()*48+48,model.getMap().getHeight()*48+48));
 		
-		/* Ajout du message de fin de partie */
+		/* End game message */
 		this.message = new JLabel();
 		this.add(message);
-		message.setBounds(624/2-40, 624/2-20,80 ,40);
+		FontRenderContext frc= DEFAULT_GRAPHICS.getFontRenderContext();
+		int width=(int)this.message.getFont().getStringBounds(message.getText(), frc).getWidth();
+		int height=(int)this.message.getFont().getStringBounds(message.getText(), frc).getHeight();
+		Rectangle rectangle = new Rectangle((624-width)/2,(624-height)/2,width,height);
+		message.setBounds(rectangle);
 		
-		/* Ajout du bouton restart */
-		restartButton = new Button("Restart");
+		/* Adding the restart button */
+		restartButton = new JButton("Restart");
+		restartButton.setFocusPainted(false);
+		restartButton.setContentAreaFilled(false);
 		restartButton.addActionListener(new ActionListener() {
 
 			@Override
@@ -56,8 +63,10 @@ public class EndGameView extends JPanel {
 		this.add(restartButton);
 		restartButton.setBounds(((model.getMap().getWidth()+1)*48-BUTTON_SIZE_WIDTH)/2, 500, BUTTON_SIZE_WIDTH, BUTTON_SIZE_HEIGHT);
 		
-		/* ajout du bouton exit */
-		exitButton = new Button("Exit");
+		/* Adding exit button */
+		exitButton = new JButton("Exit");
+		exitButton.setFocusPainted(false);
+		exitButton.setContentAreaFilled(false);
 		exitButton.addActionListener(new ActionListener() {
 
 			@Override
